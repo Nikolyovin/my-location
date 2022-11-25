cd0e58491813758ecb9d5c56a99244bbea3c370c
Лёвин Николай Сергеевич <151705@grpz.su>
2022-11-25 08:07:12 +0300

    test footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react';
-import * as Location from 'expo-location';
+// import * as Location from 'expo-location';
 import { useActions } from '../hooks/action';
 
 const Footer = () => {
@@ -12,8 +12,14 @@ const Footer = () => {
 
     return (
         <View style={styles.buttonWrap}>
+            <TouchableOpacity style={styles.button} onPress={onShowModal}>
+                <Text style={styles.buttonAddText}>Отправить на почту</Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.buttonAdd} onPress={onShowModal}>
-                <Text style={styles.buttonAddText}>Добавить точку</Text>
+                <Text style={styles.buttonAddText}>+</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={onShowModal}>
+                <Text style={styles.buttonAddText}>Удалить всё</Text>
             </TouchableOpacity>
         </View>
     )
@@ -24,17 +30,27 @@ export default Footer
 const styles = StyleSheet.create({
     buttonWrap: {
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        justifyContent: 'space-between',
+        // alignItems: 'flex-start',
         paddingHorizontal: 25,
         paddingBottom: 30
     },
-    buttonAdd: {
-        backgroundColor: '#00c68f',
-        width: '100%',
+    button: {
+        // backgroundColor: '#00c68f',
+        backgroundColor: 'red',
+        // width: '100%',
         height: 50,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 15
+    },
+    buttonAdd: {
+        backgroundColor: '#00c68f',
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50
     },
     buttonAddText: {
         color: 'white',
