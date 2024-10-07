89c723cdd8b85639bc3cd51391db7470c8db49cd
Nikolay Lyovin <pulya0763@gmail.com>
2024-10-07 20:52:42 +0300

    копирование в буфер

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,9 @@
-import { AsyncStorage, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { FC, useEffect } from 'react'
+import { AsyncStorage, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native'
+import React, { FC } from 'react'
 import { FontAwesome5 } from '@expo/vector-icons'
 import { useActions } from '../hooks/action'
 import { useAppSelector } from '../hooks/redux'
+import Clipboard from '@react-native-clipboard/clipboard'
 
 interface IProps {
     coordinates: number[]
@@ -13,6 +14,7 @@ interface IProps {
 const Card: FC<IProps> = ({ coordinates, description, id }) => {
     const { removeLocation, isShowEditMode } = useActions()
     const { isEditMode } = useAppSelector(state => state.app)
+
     const onRemove: () => void = () => {
         removeLocation(id)
     }
@@ -21,26 +23,28 @@ const Card: FC<IProps> = ({ coordinates, description, id }) => {
         isShowEditMode(true)
     }
 
+    const onPress: () => void = () => {
+        const clipboardContent = `${coordinates[0]}, ${coordinates[1]}`
+        Clipboard.setString(clipboardContent)
+        Alert.alert('Успех', 'Координаты скопированы в буфер обмена!')
+    }
+
     return (
         <View style={styles.cardContainer}>
-            <TouchableOpacity style={styles.cardWrap} onLongPress={onLongPress}>
+            <TouchableOpacity style={styles.cardWrap} onLongPress={onLongPress} onPress={onPress}>
                 <View style={styles.rightWrap}>
                     <Text style={styles.coordinates}>Широта: {coordinates[0]}</Text>
                     <Text style={styles.coordinates}>Долгота: {coordinates[1]}</Text>
                 </View>
                 <Text style={styles.description}>{description}</Text>
             </TouchableOpacity>
-            {isEditMode &&
+            {isEditMode && (
                 <View style={styles.iconWrap}>
                     <TouchableOpacity onPress={onRemove}>
-                        <FontAwesome5
-                            name={'trash-alt'}
-                            size={25}
-                            color={'red'}
-                        />
+                        <FontAwesome5 name={'trash-alt'} size={25} color={'red'} />
                     </TouchableOpacity>
                 </View>
-            }
+            )}
         </View>
     )
 }
@@ -51,7 +55,7 @@ const styles = StyleSheet.create({
     cardContainer: {
         alignItems: 'center',
         alignSelf: 'center',
-        width: '90%',
+        width: '90%'
     },
     cardWrap: {
         backgroundColor: '#dafaf0',
@@ -63,7 +67,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         paddingVertical: 20,
         marginBottom: 10,
-        borderRadius: 15,
+        borderRadius: 15
     },
     rightWrap: {
         marginRight: 15
@@ -86,6 +90,6 @@ const styles = StyleSheet.create({
         top: -5,
         height: '100%',
         // alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     }
 })
